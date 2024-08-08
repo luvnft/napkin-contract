8f84f024692f034f06ec0818287db38142c736de
qutluq <kootlook.applications@gmail.com>
2024-08-08 19:46:36 +0500

    feat: add button tag component in resuable button component

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -4,24 +4,27 @@ type PropTypes = {
   title: string;
   href?: string;
   onClick?: () => void;
+  className?: string;
 };
 
-export const Button = ({ href, title, onClick }: PropTypes) => {
+export const Button = ({
+  href,
+  title,
+  onClick,
+  className = 'bg-[#DC0203] text-white text-xl font-medium w-56 h-12 rounded-xl flex flex-row items-center justify-center cursor-pointer',
+}: PropTypes) => {
   if (!href && !onClick) return null;
 
   if (href) {
     return (
-      <Link
-        href={href}
-        className="bg-[#DC0203] text-white text-xl font-medium w-56 h-12 rounded-xl flex flex-row items-center justify-center"
-      >
+      <Link href={href} className={className}>
         {title}
       </Link>
     );
   }
 
   return (
-    <button className="text-white w-56 rounded-xl" onClick={onClick}>
+    <button className={className} onClick={onClick}>
       {title}
     </button>
   );
