4812750b2cb58068d16149a32162204ee6a43a37
qutluq <kootlook.applications@gmail.com>
2024-08-08 19:45:55 +0500

    refactor: update header with our own logo

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-
-import thirdwebIcon from "@public/thirdweb.svg";
+import { Logo } from '@/shared/icon';
 
 export const Header = () => {
   return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        <span className="text-zinc-300 inline-block mx-1">Napkin Contract</span>
-      </h1>
+    <header className="flex flex-col shrink-0 items-center justify-center border-y border-black w-full h-16">
+      <Logo height={30} />
     </header>
   );
 };
