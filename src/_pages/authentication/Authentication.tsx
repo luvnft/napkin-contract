e8e1abcc285a85ef54cbcfdfe1d5696f75c70654
qutluq <kootlook.applications@gmail.com>
2024-08-09 21:35:04 +0500

    feat: add auth page

--- src/_pages/authentication/Authentication.tsx
+++ src/_pages/authentication/Authentication.tsx
@@ -1,12 +1,23 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { ConnectButton } from 'thirdweb/react';
 
+import { Button } from '@/shared/components';
 import { MainWrapper } from '@/shared/components/main-wrapper/MainWrapper';
 import { appName, appUrl, client } from '@/shared/const';
 
 export const Authentication = () => {
+  const router = useRouter();
+  const submitButton = (
+    <Button
+      onClick={() => {
+        router.push('/contract/review', { scroll: false });
+      }}
+      title="Next"
+    />
+  );
   return (
-    <MainWrapper title="Authentication">
+    <MainWrapper title="Authentication" submitButton={submitButton}>
       <div className="flex flex-col items-center justify-start p-10">
         <ConnectButton
           client={client}
