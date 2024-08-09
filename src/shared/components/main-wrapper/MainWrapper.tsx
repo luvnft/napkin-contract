45db100c7ef2c1df8dd6e26a639211932de06526
qutluq <kootlook.applications@gmail.com>
2024-08-09 23:29:30 +0500

    style: all titles at the same position

--- src/shared/components/main-wrapper/MainWrapper.tsx
+++ src/shared/components/main-wrapper/MainWrapper.tsx
@@ -7,7 +7,7 @@ type PropTypes = {
 };
 export const MainWrapper = ({ title, children, submitButton }: PropTypes) => (
   <div className="flex flex-col w-full h-full">
-    <h3 className="h-16 flex items-center justify-center text-black text-2xl font-semibold">
+    <h3 className="h-16 flex items-start pt-6 px-3 justify-center text-black text-2xl font-semibold text-center">
       {title}
     </h3>
     <div className="flex-grow min-h-0 p-2 px-5">{children}</div>
