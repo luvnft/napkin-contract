f1a493cfbe26b239774d302ce60605a45958fd64
qutluq <kootlook.applications@gmail.com>
2024-08-10 04:53:31 +0500

    feat: tap qr code to contract address

--- src/_pages/contract/ui/ShareContract.tsx
+++ src/_pages/contract/ui/ShareContract.tsx
@@ -4,12 +4,17 @@ import { useRouter } from 'next/navigation';
 import { Button, MainWrapper } from '@/shared/components';
 import { useAppSelector } from '@/shared/store/hook';
 import { contractSelector } from '@/shared/store/selector/contract';
-import { toastSuccess } from '@/shared/utils/toast';
+import { toastInfo, toastSuccess } from '@/shared/utils/toast';
 import qrCode from '@public/qrcode.png';
 
 export const ShareContract = () => {
   const router = useRouter();
   const currentContract = useAppSelector(contractSelector);
+
+  const handleQRClick = () => {
+    window.isSecureContext && navigator.clipboard.writeText(`/contract/${currentContract.id}`);
+    toastInfo('contract link copied');
+  };
 
   const submitButton = (
     <Button
@@ -23,8 +28,14 @@ export const ShareContract = () => {
 
   return (
     <MainWrapper title="Share the QR to request signing" submitButton={submitButton}>
-      <div className="h-full sm:h-[459px] sm:max-h-[459px] w-full p-2 rounded text-black text-2xl flex items-center justify-center">
-        <Image src={qrCode} alt="" className="size-[150px] md:size-[150px]" />
+      <div className="h-full sm:h-[459px] sm:max-h-[459px] w-full p-2 rounded text-black text-2xl flex flex-col items-center justify-center gap-3">
+        <Image
+          src={qrCode}
+          alt=""
+          className="size-[150px] md:size-[150px]"
+          onClick={handleQRClick}
+        />
+        <span className="text-center text-gray-500">Tap QR code to copy contract link</span>
       </div>
     </MainWrapper>
   );
