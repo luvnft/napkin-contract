d47be8a928c51a70a0261a527ff7ebf95f834edc
Denis Perov <denis.perov@gmail.com>
2024-08-10 13:39:01 +0500

    Clean up

--- src/api/eas.ts
+++ src/api/eas.ts
@@ -2,11 +2,13 @@ import { ethers } from 'ethers';
 
 import { EAS, SchemaEncoder } from '@ethereum-attestation-service/eas-sdk';
 
+const ETHERS_NETWORK = 'https://sepolia.base.org';
+
 async function attest(encodedData: string, schemaUID: string, refUID?: string) {
-  const privateKey = process.env.PRIVATE_KEY || 'TEST_KEY';
-  const easContractAddress = process.env.EAS_CONTRACT_ADDRESS || 'TEST_ADDRESS';
+  const privateKey = process.env.PRIVATE_KEY;
+  const easContractAddress = process.env.NEXT_PUBLIC_EAS_CONTRACT_ADDRESS;
   const eas = new EAS(easContractAddress);
-  const provider = ethers.getDefaultProvider('https://sepolia.base.org');
+  const provider = ethers.getDefaultProvider(ETHERS_NETWORK);
   const signer = new ethers.Wallet(privateKey, provider);
   // Signer must be an ethers-like signer.
   eas.connect(signer);
@@ -25,7 +27,7 @@ async function attest(encodedData: string, schemaUID: string, refUID?: string) {
 }
 
 export async function createContractAttestation(drafterID: string, contract: string) {
-  const schemaUID = process.env.CONTRACT_SCHEMA_ID || 'TEST_ID';
+  const schemaUID = process.env.NEXT_PUBLIC_CONTRACT_SCHEMA_ID;
   const schemaEncoder = new SchemaEncoder('string drafterID,string contract');
   const encodedData = schemaEncoder.encodeData([
     { name: 'drafterID', value: drafterID, type: 'string' },
@@ -35,7 +37,7 @@ export async function createContractAttestation(drafterID: string, contract: str
 }
 
 export async function signContractAttestation(signatoryID: string, uidContract: string) {
-  const schemaUID = process.env.SIGNATORY_SCHEMA_ID || 'TEST_ID';
+  const schemaUID = process.env.NEXT_PUBLIC_SIGNATORY_SCHEMA_ID;
   const schemaEncoder = new SchemaEncoder('string signatoryID');
   const encodedData = schemaEncoder.encodeData([
     { name: 'signatoryID', value: signatoryID, type: 'string' },
@@ -44,19 +46,12 @@ export async function signContractAttestation(signatoryID: string, uidContract: 
 }
 
 export async function findContractAttestation(uid: string) {
-  const easContractAddress = process.env.EAS_CONTRACT_ADDRESS || 'TEST_ADDRESS';
+  const easContractAddress = process.env.NEXT_PUBLIC_EAS_CONTRACT_ADDRESS;
   const eas = new EAS(easContractAddress);
-  const provider = ethers.getDefaultProvider('https://sepolia.base.org');
+  const provider = ethers.getDefaultProvider(ETHERS_NETWORK);
   // @ts-ignore
   eas.connect(provider);
   const attestation = await eas.getAttestation(uid);
   const schemaEncoder = new SchemaEncoder('string drafterID,string contract');
   return schemaEncoder.decodeData(attestation.data);
 }
-
-// const uidContract = await createContractAttestation('0xDrafter_ID', 'Generated contract body');
-// const uidSignatory = await signContractAttestation('0xSignatory_ID', uidContract);
-// console.log(uidContract);
-// console.log(uidSignatory);
-// const attestation = await findContractAttestation('0x4a3bd04a93bbff279469ec11627233793ed130b33585243d15ca74b228a60220');
-// console.log(attestation);
