2a9d9f0f50204d69bbb4d31b712c322b7f01883e
qutluq <kootlook.applications@gmail.com>
2024-08-11 12:16:36 +0500

    feat: reset user on home page

--- src/_pages/home/Home.tsx
+++ src/_pages/home/Home.tsx
@@ -1,14 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+
 import { Button } from '@/shared/components';
 import { Logo } from '@/shared/icon';
+import { useAppDispatch } from '@/shared/store/hook';
+import { resetUser } from '@/shared/store/slice/user';
 
-export const Home = () => (
-  <main className="flex flex-col gap-7 items-center justify-center">
-    <div className="h-[150px] flex items-center justify-center">
-      <Logo />
-    </div>
+export const Home = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(resetUser());
+  }, [dispatch]);
 
-    <Button title="Create contract" href="/contract/new" />
-    <Button title="Paste code" href="/qr/paste" />
-    <Button title="Scan QR" href="/scan-qr" />
-  </main>
-);
+  return (
+    <main className="flex flex-col gap-7 items-center justify-center">
+      <div className="h-[150px] flex items-center justify-center">
+        <Logo />
+      </div>
+
+      <Button title="Create contract" href="/contract/new" />
+      <Button title="Paste code" href="/qr/paste" />
+      <Button title="Scan QR" href="/scan-qr" />
+    </main>
+  );
+};
